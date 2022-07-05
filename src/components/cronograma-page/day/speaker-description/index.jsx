46c41605b34048cc8970931e8f6cc1cb2020b37d
Thiago Fernandes <tjfctigo@gmail.com>
2022-07-05 19:24:09 -0300

    cronograma: preletores dinamicos

--- src/components/cronograma-page/day/speaker-description/index.jsx
+++ src/components/cronograma-page/day/speaker-description/index.jsx
@@ -1,24 +1,34 @@
-import speakerImg from "../../../../../public/img/preletores/adriel-souza.svg"
+import speakerInfo from "../../../../data/cults.json";
 import cronogramaPageStyles from "./index.module.scss";
+import {useRouter} from "next/router"
 
 export function SpeakerDescriptionCronogramaPage() {
+  const nextRouter = useRouter()
+  const {name, office, description, img} = getSpeakerDataByRoute() 
+
+  function getSpeakerDataByRoute() {
+    const currentRoute = nextRouter.query.day
+    const speakerData = speakerInfo[currentRoute].speaker
+    
+    return speakerData
+  }
+
+
   return (
     <section className={cronogramaPageStyles['speaker-section']}>
       <h2 className={cronogramaPageStyles['speaker']}>Preletor</h2>
-
       <div className={cronogramaPageStyles["speaker-container"]}>
-          <img className={cronogramaPageStyles["speaker-img"]} src={speakerImg.src} alt="" />
+          <img className={cronogramaPageStyles["speaker-img"]} src={img} alt="" />
 
           <div className={cronogramaPageStyles["speaker-info"]}>
-            <h3 className={cronogramaPageStyles['speaker-name']}>Pr. Adriel Souza</h3>
-            <p className={cronogramaPageStyles['speaker-description']}>
-              Lorem ipsum dolor sit amet, con
-              sectetur adipiscing elit.  Curabitur 
-              euismod euismod nibh,  quis 
-              semper risus cursus eget. Pro
-              in sit amet laoreet ex. Aenean ue 
-              enim ac porttitor molestie
-            </p>
+            <div className={cronogramaPageStyles["speaker-text-container"]}>
+              <h3 className={cronogramaPageStyles['speaker-name']}>
+                {`${office} ${name}`}
+              </h3>
+              <p className={cronogramaPageStyles['speaker-description']}>
+                {description}
+              </p>
+            </div>
 
             <button className={cronogramaPageStyles["speaker-redirector-button"]}>
             Conheça nossa equipe
